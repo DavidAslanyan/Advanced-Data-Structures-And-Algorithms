f5bc056ca060fbaa011f5c8a4b71b88cce736704
DavidAslanyan <david.aslanyan@acquerant.com>
2025-05-17 12:00:21 +0400

    grinding in leetcode

---
+++ problems/groupAnagrams.js
@@ -0,0 +1,16 @@
+const groupAnagrams = function(strs) {
+  const map = new Map();
+
+  for (let i = 0; i < strs.length; i++) {
+      const sorted = strs[i].split('').sort().join('');
+
+      if (map.get(sorted)) {
+          map.get(sorted).push(strs[i]);
+      } else {
+          map.set(sorted, [strs[i]]);
+      }
+  } 
+
+  return Array.from(map.values());
+};
+
