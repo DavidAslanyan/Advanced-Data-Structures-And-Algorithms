3cfee33a5d6c5b256d59a1a42f31e013e680f346
DavidAslanyan <david.aslanyan@acquerant.com>
2025-05-23 16:22:51 +0400

    Solving LeetCode problems

---
+++ problems/firstUniqueChar.js
@@ -0,0 +1,25 @@
+var firstUniqChar = function(s) {
+  let chars = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    let el = s[i];
+    if (chars.has(el)) {
+        let currentVal = chars.get(el);
+        chars.set(el, currentVal + 1);
+    } else {
+      chars.set(el, 0);
+    }
+  }
+
+  let uniqueChar;
+  for (const [key, value] of chars) {
+    if (value === 0) {
+      uniqueChar = key;
+      break;
+    }
+  }
+
+  return s.indexOf(uniqueChar);
+};
+
+
