7f43bfaf5be55dbbf52b0cd3ff5515f6c0e8aa2a
DavidAslanyan <david.aslanyan@acquerant.com>
2025-06-10 12:08:07 +0400

    Solving LeetCode Problems

---
+++ problems/majorityElement.js
@@ -0,0 +1,21 @@
+var majorityElement = function(nums) {
+    const map = new Map();
+
+    for (const num of nums) {
+        if (map.has(num)) {
+            map.set(num, map.get(num) + 1);
+        } else {
+            map.set(num, 1);
+        }
+    }
+
+    for (const item of map) {
+        const [key, value] = item;
+
+        if (value > nums.length / 2) {
+            return key;
+        }
+    }
+
+    return 1;
+};
