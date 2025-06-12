54a488e973c274e798e08ba4badf5fed7c605cf4
DavidAslanyan <david.aslanyan@acquerant.com>
2025-06-12 11:36:10 +0400

    Solving LeetCode Problems

---
+++ problems/findMaxAverage.js
@@ -0,0 +1,21 @@
+// Sliding Window Easy
+
+var findMaxAverage = function(nums, k) {
+    if (nums.length < k) return 0;
+
+    let maxSum = 0;
+    let windowSum = 0;
+
+    for (let i = 0; i < k; i++) {
+        windowSum += nums[i];
+    }
+
+    maxSum = windowSum / k;
+
+    for (let i = k; i < nums.length; i++) {
+        windowSum = windowSum - nums[i - k] + nums[i];
+        maxSum = Math.max(maxSum, windowSum / k);
+    }
+
+    return maxSum;
+};
