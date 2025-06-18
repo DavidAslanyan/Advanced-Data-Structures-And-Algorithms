0c7848b3d878d4d64f166b81fd7c3932455c9119
DavidAslanyan <david.aslanyan@acquerant.com>
2025-06-18 14:25:27 +0400

    Solving LeetCode Problems

---
+++ problems/isPalindromeNumber.js
@@ -0,0 +1,16 @@
+function isPalindromeNumber(x) {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+  let reversed = 0;
+  let original = x;
+
+  while (x > 0) {
+    let lastDigit = x % 10;
+    reversed = reversed * 10 + lastDigit;
+    x = Math.floor(x / 10);
+  }
+
+  return reversed === original
+}
+
+console.log(isPalindromeNumber(121))
