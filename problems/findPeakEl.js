f6a5f0f42bbcdae5f4f32e4f3bf2891f81882527
DavidAslanyan <david.aslanyan@acquerant.com>
2025-06-24 12:05:02 +0400

    Solving LeetCode Problems

--- problems/findPeakEl.js
+++ problems/findPeakEl.js
@@ -1,16 +1,15 @@
 var findPeakElement = function(nums) {
-    let left = 0; 
-    let right = nums.length - 1; 
+    let left = 0;
+    let right = nums.length - 1;
 
-    while (left < right) {  // [1, 2, 3, 1]
-        let mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] > nums[mid + 1]) {
-            right = mid; 
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (mid > 0 && nums[mid] < nums[mid - 1]) {
+            right = mid - 1;
+        } else if (mid < nums.length - 1 && nums[mid] < nums[mid + 1]) {
+            left = mid + 1;
         } else {
-            left = mid + 1; 
+            return mid;
         }
     }
-
-    return left;
 };
