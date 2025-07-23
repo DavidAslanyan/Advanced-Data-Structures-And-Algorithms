5f4af42cc2b703868a49fb79924b1de1ad118c31
David Aslanyan <89131830+DavidAslanyan@users.noreply.github.com>
2025-07-23 17:09:32 +0400

    Update productOfSelf.js

--- problems/productOfSelf.js
+++ problems/productOfSelf.js
@@ -6,7 +6,7 @@ function productExceptSelf(nums) {
     for (let i = 0; i < n; i++) {   
         result[i] = prefixProduct;
         prefixProduct *= nums[i];
-    }  
+    }     
 
     let suffixProduct = 1;
     for (let i = n - 1; i >= 0; i--) {
