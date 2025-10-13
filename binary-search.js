d559735e25ff449fcd3e953fb143bc3d2301f169
David Aslanyan <89131830+DavidAslanyan@users.noreply.github.com>
2025-10-13 15:03:12 +0400

    Update binary-search.js

--- binary-search.js
+++ binary-search.js
@@ -1,7 +1,7 @@
 const binarySearch = (arr, t) => {
   let left = 0;
   let right = arr.length - 1;
-
+  
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
     if (arr[mid] === t) {
