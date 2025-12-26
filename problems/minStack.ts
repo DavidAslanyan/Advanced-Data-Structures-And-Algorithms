08166bb4fc0213a69dab39e8a5b02910cc6b705d
DavidAslanyan <89131830+DavidAslanyan@users.noreply.github.com>
2025-12-26 14:11:31 +0400

    LeetCode Marathon

---
+++ problems/minStack.ts
@@ -0,0 +1,38 @@
+class MinStack {
+  private stack: number[] = [];
+  private minStack: number[] = [];
+
+  constructor() {
+      
+  }
+
+  push(val: number): void {
+      this.stack.push(val);
+
+      const currentMin = this.minStack.length === 0 ? val : Math.min(val, this.minStack[this.minStack.length - 1]);
+
+      this.minStack.push(currentMin);
+  }
+
+  pop(): void {
+      this.stack.pop();
+      this.minStack.pop();
+  }
+
+  top(): number {
+      return this.stack[this.stack.length - 1];
+  }
+
+  getMin(): number {
+      return this.minStack[this.minStack.length - 1];
+  }
+}
+
+/**
+* Your MinStack object will be instantiated and called as such:
+* var obj = new MinStack()
+* obj.push(val)
+* obj.pop()
+* var param_3 = obj.top()
+* var param_4 = obj.getMin()
+*/
